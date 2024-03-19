8fff95af8a1ca0cd121db6a5b4f25bcf4b66c351
Fabricio Dias <fabricio270909hotmail.com>
2024-03-19 14:49:10 -0300

    Added edit value to input

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
 import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
@@ -6,11 +7,19 @@ import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
+  @Input() itemToEdit!: Item;
   valorItem! : string;
   constructor(private listaService: ListaDeCompraService) { }
 
   ngOnInit(): void { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(!changes['itemToEdit'].firstChange){
+      this.valorItem = changes['itemToEdit'].currentValue.nome;
+    }
+
+  }
 
   adicionarItem() {
     this.listaService.adicionarItemNaLista(this.valorItem);
